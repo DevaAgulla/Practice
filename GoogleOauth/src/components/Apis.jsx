19a4c26d198133ea85baa0abb76a3c3cd246f874
DevaAgulla <87611913+DevaAgulla@users.noreply.github.com>
2024-06-06 12:24:41 +0530

    Delete GoogleOauth/src directory

--- GoogleOauth/src/components/Apis.jsx
+++
@@ -1,6 +0,0 @@
-import { useOutletContext } from "react-router-dom";
-
-export const Apis = ()=>{
-    let obj = useOutletContext();
-    return <h1>Apis {obj[0]}</h1>;
-}
--- GoogleOauth/src/components/Cart.jsx
+++
@@ -1,7 +0,0 @@
-
-import { useOutletContext } from "react-router-dom";
-export const Cart = ()=>{
-    let obj = useOutletContext();
-    console.log(obj)
-    return <h1>Cart {obj[1]}</h1>;
-}
